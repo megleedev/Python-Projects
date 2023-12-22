579b3e9d20d06de9f8f9b4e8e4ae1554d51fc869
Meg Lee <megleedev@outlook.com>
2023-12-22 01:11:40 -0800

    Update Inheritance.py

--- Inheritance.py
+++ Inheritance.py
@@ -1,23 +1,56 @@
 # Creates a class called 'Animal'
-# Animal is created with two arguments for creating an Animal object
+# Animal is created with two variable names for attributes with default values
 # Animal is also the Parent class for other classes
 class Animal:
-    def __init__(self, species, predator):
-        self.species = species
-        self.predator = True
-        
+    species = " "
+    predator = True
 
 # Creates a Bird class that inherits from the Animal parent class
-# Bird is created with two arguments for creating a Bird object
+# Bird is created with two variable names for attributes with default values
 class Bird (Animal):
-    def __init__(self, color, size):
-        self.color = color
-        self.size = size
+    color = " "
+    size = " "
 
 # Creates an Eagle class that inherits from both the Animal parent class -
 # and the Bird class
-# Eagle is created with two arguments for creating a Eagle object
-class Eagle (Bird, Animal):
-    def __init__(self, weight, height):
-        self.weight = 0.00
-        self.height = 0
+# Eagle is created with two variable names for attributes with default values
+class LargeBird (Bird, Animal):
+    weight = 0.00
+    height = 0
+
+# I got concerned that the above didn't work so I made sure
+if __name__ == "__main__":
+
+    LargeCat = Animal ()
+    LargeCat.species = "Tiger"
+    LargeCat.predator = True
+
+    print (LargeCat.species)
+    print (LargeCat.predator)
+
+
+    SmallBird = Bird ()
+    SmallBird.species = "Pidgeon"
+    SmallBird.predator = False
+    SmallBird.color = "White"
+    SmallBird.size = "Small"
+
+    print (SmallBird.species)
+    print (SmallBird.predator)
+    print (SmallBird.color)
+    print (SmallBird.size)
+
+    Eagle = LargeBird ()
+    Eagle.species = "Eagle"
+    Eagle.predator = True
+    Eagle.color = "Brown/White"
+    Eagle.size = "Large"
+    Eagle.weight = 13.5
+    Eagle.height = 30
+
+    print (Eagle.species)
+    print (Eagle.predator)
+    print (Eagle.color)
+    print (Eagle.size)
+    print (Eagle.weight)
+    print (Eagle.height)
